86adedf9faea2cb11c06cdd21b2f065a7828c278
laks <laks@gmail.com>
2023-03-15 17:21:22 +0700

    posisi karakter game

---
+++ modul4_1302210025/PosisiKarakterGame.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302210025
+{
+    public enum posisi { berdiri, terbang, jongkok, tengkurap }
+    public enum triger { tombolW, tombolS, tombolX }
+    internal class PosisiKarakterGame
+    {
+        class moveState
+        {
+
+            public posisi curr, next;
+            public triger tombol;
+            public moveState(posisi curr, posisi next, triger tombol)
+            {
+                this.curr = curr;
+                this.next = next;
+                this.tombol = tombol;
+            }
+        }
+        posisi mycurr = posisi.berdiri;
+        triger mytriger;
+        moveState[] move = 
+        {
+       new moveState(posisi.berdiri,
+                               posisi.terbang,
+                               triger.tombolW),
+        new moveState(posisi.terbang,
+                                   posisi.jongkok,
+                                   triger.tombolX),
+           new moveState(posisi.jongkok,
+                                   posisi.tengkurap,
+                                   triger.tombolS),
+           new moveState(posisi.tengkurap,
+                                   posisi.jongkok,
+                                   triger.tombolW),
+           new moveState(posisi.jongkok,
+                                   posisi.berdiri,
+                                   triger.tombolW),
+           new moveState(posisi.berdiri,
+                                   posisi.jongkok,
+                                   triger.tombolS),
+           new moveState(posisi.terbang,
+                                   posisi.berdiri,
+                                   triger.tombolS)
+        };
+        public PosisiKarakterGame()
+        {
+            Console.WriteLine("Posisi Sekarang " + mycurr.ToString() + ", posisi stand by");  
+        }
+        public void setMyTriger(triger t)
+        {
+            mytriger = t;
+            getMyPosition();
+        }
+        public void getMyPosition()
+        {
+            for(var i = 0; i < move.Length; i++)
+            {
+               moveState tmp = move[i];
+               if(tmp.tombol == mytriger &&tmp.curr == mycurr)
+                {
+                    if(tmp.next.ToString() == "berdiri")
+                    {
+                        Console.WriteLine("posisi stand by");
+                    }else if(tmp.next.ToString() == "tengkurap")
+                    {
+                        Console.WriteLine("posisi istiarahat");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Move, current position : " + tmp.next.ToString());
+                    }
+                    mycurr= tmp.next;
+                }
+            }
+        }
+
+       
+        //add all state
+       
+     }
+}
--- modul4_1302210025/Program.cs
+++ modul4_1302210025/Program.cs
@@ -4,3 +4,28 @@ using modul4_1302210025;
 
 KodeBuah kb = new KodeBuah();
 Console.WriteLine("Kode Buah " + KodeBuah.buah.jagung + " adalah " + kb.getKodeBuah(KodeBuah.buah.jagung));
+
+
+Console.WriteLine();
+PosisiKarakterGame p = new PosisiKarakterGame(); 
+Console.WriteLine("Pilih Tombol");
+Console.WriteLine("1. tombolW");
+Console.WriteLine("2. tombolS");
+Console.WriteLine("3. tombolx");
+var myt = Console.ReadLine();
+while (myt != "0") { 
+    if(myt == "1")
+    {
+        p.setMyTriger(triger.tombolW);
+    }
+    else if(myt == "2")
+    {
+        p.setMyTriger(triger.tombolS);
+    }
+    else
+    {
+        p.setMyTriger(triger.tombolX);
+    }
+    myt = Console.ReadLine();
+}
+
