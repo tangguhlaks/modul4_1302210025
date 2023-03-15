2c6f5aa4e6205898b681798e65e294dd133d2fb3
laks <laks@gmail.com>
2023-03-15 16:08:10 +0700

    task 1 table driven

---
+++ modul4_1302210025/KodeBuah.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302210025
+{
+    internal class KodeBuah
+    {
+        public enum buah { apel,aprikot,alpukat,pisang,paprika,blackbarry,ceri,kelapa, jagung }
+        String[] arr = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00" };
+
+        public string getKodeBuah(buah namabuah) => arr[(int) namabuah];
+    }
+}
---
+++ modul4_1302210025/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using modul4_1302210025;
+
+
+KodeBuah kb = new KodeBuah();
+Console.WriteLine("Kode Buah " + KodeBuah.buah.jagung + " adalah " + kb.getKodeBuah(KodeBuah.buah.jagung));
